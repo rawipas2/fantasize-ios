be8a988a4476808e852e7d5d0c630cd32e1b6770
rawipas41 <rawipas_s@flowaccount.com>
2022-12-15 12:41:20 +0700

    first commit

---
+++ fantasize-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fantasize-ios
+//
+//  Created by Rawipas Samoondee on 15/12/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
